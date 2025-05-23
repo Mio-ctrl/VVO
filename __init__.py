6be1b1b13c1a74d884510946bc4bba0846ce7718
Mio-ctrl <158206744+Mio-ctrl@users.noreply.github.com>
2025-05-24 00:44:33 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+from homeassistant.helpers.discovery import async_load_platform
 
+async def async_setup(hass, config):
+    # Wir registrieren nur die Plattform "sensor"
+    hass.async_create_task(
+        async_load_platform(hass, "sensor", "vvo", {}, config)
+    )
+    return True
